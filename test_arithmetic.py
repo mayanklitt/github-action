be5c0a2c1a4e69d16b6c79818adc414280cb020e
mayanklitt <mayankkumar1@dcmail.ca>
2024-07-05 15:03:19 -0400

    commi2

--- test_arithmetic.py
+++ test_arithmetic.py
@@ -1,20 +1,23 @@
-# test_arithmetic.py initial file
-
 import pytest
 from arithmetic import add, subtract, multiply, divide
 
 def test_add():
+    """Test the add function"""
     assert add(1, 2) == 3
 
 def test_subtract():
+    """Test the subtract function"""
     assert subtract(2, 1) == 1
 
 def test_multiply():
+    """Test the multiply function"""
     assert multiply(2, 3) == 6
 
 def test_divide():
+    """Test the divide function"""
     assert divide(6, 2) == 3
 
 def test_divide_by_zero():
+    """Test divide by zero raises ValueError"""
     with pytest.raises(ValueError):
         divide(1, 0)
